3547910f46f98794ff20fe0cf793463bb2932af6
Erhan Abibula <erhan.abibula@gmail.com>
2019-09-16 17:39:44 +0300

    work on demo app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,29 +1,81 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-import ExampleComponent from "react-menu-tree";
+import { Menu } from "react-menu-tree";
+
+const dataModel = [
+  {
+    title: "Getting started",
+    url: "getting-started",
+    children: [
+      { title: "First", url: "getting-started/first" },
+      { title: "Installation", url: "getting-started/installation" },
+      { title: "Site preparation", url: "getting-started/site-preparation" },
+      {
+        title: "Creating your site",
+        url: "getting-started/creating-your-site"
+      },
+      {
+        title: "Publishing your site",
+        url: "getting-started/publishing-your-site"
+      }
+    ]
+  },
+  {
+    title: "Guides",
+    url: "guides",
+    children: [
+      { title: "Adding a Blog", url: "guides/adding-a-blog" },
+      {
+        title: "Custom Pages",
+        url: "guides/custom-pages",
+        children: [{ title: "First", url: "guides/custom-pages/first" }]
+      },
+      { title: "Enabling Search", url: "guides/enabling-search" },
+      {
+        title: "Navigation and Sidebars",
+        url: "guides/navigation-and-sidebars"
+      },
+      {
+        title: "Translations & Localization",
+        url: "guides/trans-and-nav"
+      },
+      { title: "Versioning", url: "guides/versioning" }
+    ]
+  },
+  {
+    title: "API",
+    url: "api",
+    children: [
+      { title: "CLI Commands", url: "api/cli-commands" },
+      {
+        title: "Arguments",
+        url: "cli/args",
+        children: [
+          { title: "port", url: "cli/args/port" },
+          { title: "start", url: "cli/args/start" },
+          {
+            title: "stop",
+            url: "cli/args/stop",
+            children: [{ title: "First", url: "cli/args/stop/first" }]
+          }
+        ]
+      },
+      { title: "Commands", url: "cli/cmds" }
+    ]
+  }
+];
+
+const currentLocation = () => document.location.pathname;
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App-Container">
-        <ExampleComponent text="what is up bud?" />
+      <div className="App-header centered-content">Header...</div>
+      <div className="App-sidebar">
+        <Menu options={dataModel} selectedOption={currentLocation()} />
       </div>
+      <div className="App-content centered-content">Content...</div>
     </div>
   );
 };
